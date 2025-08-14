2fcdc539531a99ff2e2ca5af2640ddb9608edb28
Aditya Srivastava <adityasri70507@gmail.com>
2025-08-15 00:25:29 +0530

    Add files via upload

---
+++ Lecture2/code.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Aditya\n Srivastava" << endl;
+    return 0;
+}
+
+// boilerplate code for C++ program
+// This code prints the name "Aditya Srivastava" to the console.
+
