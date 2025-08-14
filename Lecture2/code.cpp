57b359476423a67babc2724c818c58c79bdff41e
Aditya Srivastava <adityasri70507@gmail.com>
2025-08-15 00:31:22 +0530

    Add files via upload

---
+++ Lecture2/code.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    char grade = 'A';
+    int value = grade;
+    cout << "The ASCII value of grade '" << grade << "' is: " << value << endl;
+
+    double price = 100.99;
+
+    int newPrice = (int)price;
+    cout << newPrice << endl;
+
+    return 0;
+}
+
