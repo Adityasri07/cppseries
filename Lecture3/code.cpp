21589a55aa4a7054cfd4b80403f7464802a3b21b
Aditya Srivastava <adityasri70507@gmail.com>
2025-08-17 01:23:27 +0530

    Add files via upload

---
+++ Lecture3/code.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n= 50;
+  int sum = 0;
+  int i = 1;
+
+  while(i<=n){
+    sum += i;
+    i++;
+  }
+  cout << "The sum of the first " << n << " natural numbers is: " << sum << endl;
+    
+return 0;
+
+}
