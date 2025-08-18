430be5577b78f20ae1b90b9f43592f2ad1a27cb8
Aditya Srivastava <adityasri70507@gmail.com>
2025-08-19 01:21:53 +0530

    Add files via upload

---
+++ Lecture5/code.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+//function definition
+int sumN(int n){ //parameters
+  int sum = 0; 
+  for (int i=1; i<=n;i++){
+    sum += i; //sum = sum + i
+  }
+  return sum; //returning the sum
+}
+  int main() {
+   cout << sumN(5) << endl; //function call
+   cout << sumN(10) << endl; //function call
+   return 0;
+}
