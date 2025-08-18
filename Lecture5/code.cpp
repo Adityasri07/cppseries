ac03ec8532db783f194c3e812cbd1a6f52c01354
Aditya Srivastava <adityasri70507@gmail.com>
2025-08-19 00:58:05 +0530

    Add files via upload

---
+++ Lecture5/code.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+//function definition
+int printHello(){
+    cout << "Hello, World!" << endl;
+    return 3;
+}
+int main() {
+    //function call/invoke
+  int val =  printHello();
+    cout << "The value returned by printHello is: " << val << endl;
+    cout << printHello() << endl;
+   return 0;
+}
